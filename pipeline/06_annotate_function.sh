2de2e8ac2e8eafed11251da15bfc1be774ed8148
Kian Kelly <121409271+G-gnome@users.noreply.github.com>
2023-12-15 20:30:18 -0700

    Update 06_annotate_function.sh

--- pipeline/06_annotate_function.sh
+++ pipeline/06_annotate_function.sh
@@ -1,66 +1,63 @@
-#!/usr/bin/bash -l
+#!/bin/bash -l
 #SBATCH --nodes=1
-#SBATCH --ntasks=16 --mem 16gb
+#SBATCH --ntasks=20 --mem=96G
 #SBATCH --output=logs/annotfunc.%a.log
 #SBATCH --time=2-0:00:00
 #SBATCH -p intel -J annotfunc
 
+# Load/unload modules
 module unload miniconda2 miniconda3 perl python
 module load funannotate
 module load phobius
 
+# Set environment variables
 export FUNANNOTATE_DB=/bigdata/stajichlab/shared/lib/funannotate_db
 CPUS=$SLURM_CPUS_ON_NODE
-OUTDIR=annotate
-INDIR=genomes
-SAMPFILE=samples.csv
+OUTDIR=annotation
+INDIR=genomes_to_annotate
+SAMPFILE=metadata.tsv
 BUSCO=fungi_odb10
 
-if [ -z $CPUS ]; then
+# Set default value for CPUS if not provided
+if [ -z "$CPUS" ]; then
   CPUS=1
 fi
 
+# Get the task ID from SLURM_ARRAY_TASK_ID or command line
 N=${SLURM_ARRAY_TASK_ID}
-
-if [ -z $N ]; then
+if [ -z "$N" ]; then
   N=$1
-  if [ -z $N ]; then
-    echo "need to provide a number by --array or cmdline"
-    exit
+  if [ -z "$N" ]; then
+    echo "ERROR: Need to provide a number using --array or cmdline"
+    exit 1
   fi
 fi
-MAX=`wc -l $SAMPFILE | awk '{print $1}'`
 
-if [ $N -gt $MAX ]; then
-  echo "$N is too big, only $MAX lines in $SAMPFILE"
-  exit
+# Calculate the maximum number of lines in the file
+MAX=$(wc -l < "$SAMPFILE")
+
+# Check if N is within a valid range
+if [ "$N" -gt "$MAX" ]; then
+  echo "ERROR: $N is too big, only $MAX lines in $SAMPFILE"
+  exit 1
 fi
-IFS=,
-tail -n +2 $SAMPFILE | sed -n ${N}p | while read SPECIES STRAIN PHYLUM BIOSAMPLE BIOPROJECT LOCUSTAG
+
+# Read the specified line from the SAMPFILE
+IFS=$'\t'
+tail -n +2 "$SAMPFILE" | sed -n "${N}p" | while read ASSEMBLY ACCESSION ORGANISM_NAME _ STRAIN _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 do
-  BASE=$(echo -n "$SPECIES $STRAIN" | perl -p -e 's/\s+/_/g')
-  STRAIN_NOSPACE=$(echo -n "$STRAIN" | perl -p -e 's/\s+/_/g')
-  echo "$BASE"
-  MASKED=$(realpath $INDIR/$BASE.masked.fasta)
-  if [ ! -f $MASKED ]; then
-    echo "Cannot find $BASE.masked.fasta in $INDIR - may not have been run yet"
-    exit
+  # Replace spaces with underscores in variables
+  SANITIZED_ORGANISM_NAME=$(echo "$ORGANISM_NAME" | tr ' ' '_')
+  SANITIZED_ASSEMBLY=$(echo "$ASSEMBLY" | tr ' ' '_')
+  name=$(echo -n "${SANITIZED_ORGANISM_NAME}_${SANITIZED_ASSEMBLY}" | perl -p -e 's/\s+/_/g')
+  MASKED=$(realpath "$INDIR/$name.sorted.fasta.masked")
+
+  # Check if MASKED file exists
+  if [ ! -f "$MASKED" ]; then
+    echo "ERROR: Cannot find $BASE.masked.fasta in $INDIR - may not have been run yet"
+    exit 1
   fi
-  TEMPLATE=$(realpath lib/sbt/$STRAIN_NOSPACE.sbt)
-  if [ ! -f $TEMPLATE ]; then
-    echo "NO TEMPLATE for $name"
-    exit
-  fi
-  ANTISMASHRESULT=$OUTDIR/$name/annotate_misc/antiSMASH.results.gbk
-  echo "$name $species"
-  if [[ ! -f $ANTISMASHRESULT && -d $OUTDIR/$name/antismash_local ]]; then
-    ANTISMASH=$OUTDIR/$name/antismash_local/${SPECIES}_$name.gbk
-    if [ ! -f $ANTISMASH ]; then
-      echo "CANNOT FIND $ANTISMASH in $OUTDIR/$name/antismash_local"
-    else
-      rsync -a $ANTISMASH $ANTISMASHRESULT
-    fi
-  fi
-  # need to add detect for antismash and then add that
-  funannotate annotate --sbt $TEMPLATE --busco_db $BUSCO -i $OUTDIR/$BASE --species "$SPECIES" --strain "$STRAIN" --cpus $CPUS $MOREFEATURE $EXTRAANNOT
+
+  # Run funannotate annotate
+  funannotate annotate --busco_db "$BUSCO" -i "$OUTDIR/$name" --species "$SANITIZED_ORGANISM_NAME" --strain "$STRAIN" --cpus "$CPUS" 
 done
