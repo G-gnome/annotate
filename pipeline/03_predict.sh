0451f0005009b5519c500652bb3866750468e00b
Kian Kelly <121409271+G-gnome@users.noreply.github.com>
2023-12-15 20:27:16 -0700

    Update 03_predict.sh

--- pipeline/03_predict.sh
+++ pipeline/03_predict.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/bash -l
-#SBATCH -p batch --time 3-0:00:00 --ntasks 16 --nodes 1 --mem 24G --out logs/predict.%a.log
+#SBATCH -p batch --time 3-0:00:00 --ntasks 21 --nodes 1 --mem 48G --out logs/annotate_predict.%a.log
 
 module load funannotate
 
-# this will define $SCRATCH variable if you don't have this on your system you can basically do this depending on
+# This will define $SCRATCH variable if you don't have this on your system you can basically do this depending on
 # where you have temp storage space and fast disks
-# SCRATCH=/tmp/${USER}_$$
-# mkdir -p $SCRATCH 
 module load workspace/scratch
 
 CPU=1
@@ -14,54 +12,56 @@ if [ $SLURM_CPUS_ON_NODE ]; then
     CPU=$SLURM_CPUS_ON_NODE
 fi
 
-BUSCO=fungi_odb10 # This could be changed to the core BUSCO set you want to use
-INDIR=genomes
-OUTDIR=annotate
-PREDS=$(realpath prediction_support)
+BUSCO=basidiomycota_odb10 # This could be changed to the core BUSCO set you want to use
+INDIR=genomes_to_annotate
+OUTDIR=annotation
 mkdir -p $OUTDIR
-SAMPFILE=samples.csv
+SAMPFILE=metadata.tsv
+
 N=${SLURM_ARRAY_TASK_ID}
 
-if [ ! $N ]; then
+if [ -z $N ]; then
     N=$1
-    if [ ! $N ]; then
+    if [ -z $N ]; then
         echo "need to provide a number by --array or cmdline"
         exit
     fi
 fi
-MAX=`wc -l $SAMPFILE | awk '{print $1}'`
+MAX=$(wc -l $SAMPFILE | awk '{print $1}')
 
 if [ $N -gt $MAX ]; then
     echo "$N is too big, only $MAX lines in $SAMPFILE"
     exit
 fi
 
-export AUGUSTUS_CONFIG_PATH=$(realpath lib/augustus/3.3/config)
-
+#export AUGUSTUS_CONFIG_PATH=$(realpath lib/augustus/3.3/config)
 export FUNANNOTATE_DB=/bigdata/stajichlab/shared/lib/funannotate_db
 
-SEED_SPECIES=anidulans
-
-IFS=,
-tail -n +2 $SAMPFILE | sed -n ${N}p | while read SPECIES STRAIN PHYLUM BIOSAMPLE BIOPROJECT LOCUSTAG
+SEED_SPECIES=ustilago
+SEQCENTER=UCR
+IFS=$'\t'
+tail -n +2 $SAMPFILE | sed -n ${N}p | while read ASSEMBLY ACCESSION ORGANISM_NAME _ STRAIN _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 do
-    SEQCENTER=JGI
-    BASE=$(echo -n "$SPECIES $STRAIN" | perl -p -e 's/\s+/_/g')
-    echo "sample is $BASE"
-    MASKED=$(realpath $INDIR/$BASE.masked.fasta)
+    # Replace spaces with underscores in variables
+    SANITIZED_ASSEMBLY=$(echo "$ASSEMBLY" | tr ' ' '_')
+    SANITIZED_ORGANISM_NAME=$(echo "$ORGANISM_NAME" | tr ' ' '_')
+    name=$(echo -n ${SANITIZED_ORGANISM_NAME}_${SANITIZED_ASSEMBLY} | perl -p -e 's/\s+/_/g')
+
+    MASKED=$INDIR/${name}.sorted.fasta.masked
+    echo "masked is $MASKED ($INDIR/${name}.sorted.fasta.masked)"
     if [ ! -f $MASKED ]; then
-      echo "Cannot find $BASE.masked.fasta in $INDIR - may not have been run yet"
-      exit
+        echo "no masked file $MASKED"
+        exit
     fi
-    if [[ -f $PREDS/$BASE.genemark.gtf ]]; then
-    funannotate predict --cpus $CPU --keep_no_stops --SeqCenter $SEQCENTER --busco_db $BUSCO --optimize_augustus \
-        --strain $STRAIN --min_training_models 100 --AUGUSTUS_CONFIG_PATH $AUGUSTUS_CONFIG_PATH \
-        -i $INDIR/$BASE.masked.fasta --name $LOCUSTAG --protein_evidence lib/informant.aa \
-        -s "$SPECIES"  -o $OUTDIR/$BASE --busco_seed_species $SEED_SPECIES --genemark_gtf $PREDS/$BASE.genemark.gtf
-    else
-    funannotate predict --cpus $CPU --keep_no_stops --SeqCenter $SEQCENTER --busco_db $BUSCO --optimize_augustus \
-	--strain $STRAIN --min_training_models 100 --AUGUSTUS_CONFIG_PATH $AUGUSTUS_CONFIG_PATH \
-	-i $INDIR/$BASE.masked.fasta --name $LOCUSTAG --protein_evidence lib/informant.aa \
-	-s "$SPECIES"  -o $OUTDIR/$BASE --busco_seed_species $SEED_SPECIES --tmpdir $SCRATCH
-    fi
+    echo "MASKED is $MASKED"
+
+    time funannotate predict --cpus $CPU --keep_no_stops --SeqCenter $SEQCENTER \
+                --busco_db $BUSCO --header_length 18 \
+                --strain $STRAIN --min_training_models 100 \
+                --AUGUSTUS_CONFIG_PATH $AUGUSTUS_CONFIG_PATH \
+                -i $MASKED --name $name \
+                --protein_evidence $FUNANNOTATE_DB/uniprot_sprot.fasta \
+                -s "$SANITIZED_ORGANISM_NAME" -o $OUTDIR/${name} --tmpdir $SCRATCH \
+    --force
+    #--busco_seed_species $SEED_SPECIES
 done
